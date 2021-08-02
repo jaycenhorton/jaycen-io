ea994dabd2fe9128c2233518df5dd78f3017b7c0
jaycenhorton <jaycen.horton@gmail.com>
2021-08-02 07:04:30 -0700

    add google analytics and tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,12 +50,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -74,6 +68,17 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-FKV2PK49N8", // Google Analytics / GA
+        ],
+        pluginConfig: {
+          head: false,
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
